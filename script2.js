da9976b7a4f9b98414eea98dfa86db1e659d4cb4
nandanv2702 <nandanv2702@gmail.com>
2021-07-27 08:33:28 -0400

    working sheet output

--- script2.js
+++ script2.js
@@ -16,10 +16,18 @@ var interval = setInterval(function() {
             handleFiles(e)
             .then(res => {
                 Promise.all(res).then(data => {
-                    
+                    const sheet_results = [];
+
                     data.forEach(workbook => {
-                        let sheets = workbook.SheetNames
+                        let sheets = workbook.SheetNames;
+
+                        sheets.map(sheet => {
+                            console.log(`sheetname is ${sheet}`)
+                            sheet_results.push(readSheet(workbook.Sheets[sheet]))
+                        });
                     });
+
+                    Promise.all(sheet_results).then(res => console.log(res))
 
                 });
             });
@@ -62,6 +70,42 @@ async function handleFiles(e){
 
 };
 
-function readWorkbook(workbook){
+async function readSheet(worksheet){
+    const promise = new Promise((resolve, reject) => {
 
+        try {
+            const sheet_rows = [];
+
+        console.log('readaing sheet');
+        
+        let formatted_data = XLSX.utils.sheet_to_json(worksheet, {header: 1, raw:false});
+
+        let start_idx;
+
+        formatted_data.some((elem, index) => {
+            if(elem == "AGC ISSUES"){
+                start_idx = index + 2
+            };
+        });
+
+        for(i=start_idx; i < formatted_data.length; i++){
+            if(formatted_data[i][0] == null){
+                break;
+            };
+
+            let raw_row = formatted_data[i]
+
+            let row = [raw_row[2],raw_row[3],raw_row[5],raw_row[6],raw_row[9], raw_row[12], raw_row[14], raw_row[16], raw_row[17], raw_row[18]]
+            row.map(elem => (elem == null || undefined) ? elem = "N/A" : elem = elem.toUpperCase())
+            
+            sheet_rows.push(row)
+            
+        };
+
+        resolve(sheet_rows);
+        } catch(error){
+            reject(error)
+        }
+    });  
+    return promise;
 };
