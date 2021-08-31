02db9db17de05107b7e44c5b7256b5f37879eae2
nandanv2702 <nandanv2702@gmail.com>
2021-08-31 11:22:46 -0400

    sortable by week

--- script.js
+++ script.js
@@ -146,6 +146,8 @@ var interval = setInterval(function () {
                                     Location, Issue, Downtime, RootCause, Reason, CorrectiveAction, Owner, Shift, Date, Time
                                 }))
 
+                                const dates = []
+
                                 pivotTableRes.map(row => {
                                     areaMapper(row)
                                     row['Issue'].trim()
@@ -155,9 +157,11 @@ var interval = setInterval(function () {
                                     Object.defineProperty(row, 'Downtime (s)',
                                         Object.getOwnPropertyDescriptor(row, 'Downtime'));
                                     delete row['Downtime'];
+
+                                    row['Week'] = getWeek(new Date(row['Date']))
                                 })
 
-                                console.log();
+                                console.log(`dates are ${JSON.stringify(dates)}`);
 
                                 var derivers = $.pivotUtilities.derivers;
                                 var renderers = $.extend($.pivotUtilities.renderers,
@@ -170,6 +174,7 @@ var interval = setInterval(function () {
                                     rowOrder: "value_a_to_z",
                                     colOrder: "value_z_to_a",
                                     aggregatorName: "Sum",
+                                    hiddenAttributes: ["Time"],
                                     rendererName: "Stacked Bar Chart",
                                     rendererOptions: {
                                         c3: {
@@ -386,6 +391,31 @@ function createChart(final_render, percentages) {
     });
 
 }
+
+function getWeek(date, dowOffset = 0) {
+    /*getWeek() was developed by Nick Baicoianu at MeanFreePath: http://www.meanfreepath.com */
+    const newYear = new Date(date.getFullYear(), 0, 1);
+    let day = newYear.getDay() - dowOffset; //the day of week the year begins on
+    day = (day >= 0 ? day : day + 7);
+    const daynum = Math.floor((date.getTime() - newYear.getTime() -
+      (date.getTimezoneOffset() - newYear.getTimezoneOffset()) * 60000) / 86400000) + 1;
+    //if the year starts before the middle of a week
+    if (day < 4) {
+      const weeknum = Math.floor((daynum + day - 1) / 7) + 1;
+      if (weeknum > 52) {
+        const nYear = new Date(date.getFullYear() + 1, 0, 1);
+        let nday = nYear.getDay() - dowOffset;
+        nday = nday >= 0 ? nday : nday + 7;
+        /*if the next year starts before the middle of
+          the week, it is week #1 of that year*/
+        return nday < 4 ? 1 : 53;
+      }
+      return weeknum;
+    }
+    else {
+      return Math.floor((daynum + day - 1) / 7);
+    }
+  }
 
 function areaMapper(row) {
     const mapper = {
